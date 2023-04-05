3534164e2ddd97fbea6ba3ab0a66b5299c339e78
Sohel Sk <91682695+SohelSk9732@users.noreply.github.com>
2023-04-05 13:58:35 +0530

    Update extra.py

--- Adarsh/bot/plugins/extra.py
+++ Adarsh/bot/plugins/extra.py
@@ -30,7 +30,7 @@ async def maintainers(b,m):
                     disable_web_page_preview=True)
             
          
-@StreamBot.on_message(filters.regex("Subscribe ❤️"))
+@StreamBot.on_message(filters.regex("Sᴜʙsᴄʀɪʙᴇ ❤️"))
 async def follow_user(b,m):
     try:
        await b.send_message(chat_id=m.chat.id,text="HELLO",quote=True)
