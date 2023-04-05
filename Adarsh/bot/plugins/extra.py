76d1c3fd75672eb63ce1e77ef380c0f62fce8b84
Sohel Sk <91682695+SohelSk9732@users.noreply.github.com>
2023-04-05 15:24:42 +0530

    Update extra.py

--- Adarsh/bot/plugins/extra.py
+++ Adarsh/bot/plugins/extra.py
@@ -58,16 +58,6 @@ async def start(bot, update):
         disable_web_page_preview=True,
         quote=True
     )
-
-    
-    
-@StreamBot.on_message(filters.command("list"))
-async def list(l, m):
-    LIST_MSG = "Hi! {} Here is a list of all my commands \n \n 1 . `start⚡️` \n 2. `help📚` \n 3. `login🔑` \n 4.`Subscribe ❤️` \n 5. `ping📡` \n 6. `status📊` \n 7. `DC` this tells your telegram dc \n 8. `maintainers😎` "
-    await l.send_message(chat_id = m.chat.id,
-        text = LIST_MSG.format(m.from_user.mention(style="md"))
-        
-    )
     
     
 @StreamBot.on_message(filters.regex("📡 Pɪɴɢ"))
