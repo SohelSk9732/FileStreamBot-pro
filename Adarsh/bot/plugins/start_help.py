7e8d6225af5d4d0abcaf8851fedc673ace74571f
Sohel Sk <91682695+SohelSk9732@users.noreply.github.com>
2023-04-05 15:01:32 +0530

    Update start_help.py

--- Adarsh/bot/plugins/start_help.py
+++ Adarsh/bot/plugins/start_help.py
@@ -122,13 +122,13 @@ async def help_handler(bot, message):
                 disable_web_page_preview=True)
             return
     await message.reply_text(
-        text="""<b>» Sᴇɴᴅ ᴍᴇ ᴀɴʏ ꜰɪʟᴇ (ᴏʀ) ᴍᴇᴅɪᴀ ꜰʀᴏᴍ ᴛᴇʟᴇɢʀᴀᴍ.</b>\n<b>» I ᴡɪʟʟ ᴘʀᴏᴠɪᴅᴇ ᴇxᴛᴇʀɴᴀʟ ᴅɪʀᴇᴄᴛ ᴅᴏᴡɴʟᴏᴀᴅ ʟɪɴᴋ !.</b>\n<b>» ᴅᴏᴡɴʟᴏᴀᴅ Lɪɴᴋ Wɪᴛʜ Fᴀsᴛᴇsᴛ Sᴘᴇᴇᴅ</b>\n\n🔸 𝗪𝗔𝗥𝗡𝗜𝗡𝗚 🚸\n<b>🔞 Pʀᴏɴ ᴄᴏɴᴛᴇɴᴛꜱ ʟᴇᴀᴅꜱ ᴛᴏ ᴘᴇʀᴍᴀɴᴇɴᴛ ʙᴀɴ ʏᴏᴜ.</b>\n\n<b>Aʟsᴏ  Gᴇᴛ Aʟʟ Cᴏᴍᴍᴀɴᴅs Jᴜsᴛ Sᴇɴᴅ.</b> /Link""",
+        text="""<b>» Sᴇɴᴅ ᴍᴇ ᴀɴʏ ꜰɪʟᴇ (ᴏʀ) ᴍᴇᴅɪᴀ ꜰʀᴏᴍ ᴛᴇʟᴇɢʀᴀᴍ.</b>\n<b>» I ᴡɪʟʟ ᴘʀᴏᴠɪᴅᴇ ᴇxᴛᴇʀɴᴀʟ ᴅɪʀᴇᴄᴛ ᴅᴏᴡɴʟᴏᴀᴅ ʟɪɴᴋ !.</b>\n<b>» ᴅᴏᴡɴʟᴏᴀᴅ Lɪɴᴋ Wɪᴛʜ Fᴀsᴛᴇsᴛ Sᴘᴇᴇᴅ</b>\n\n🔸 𝗪𝗔𝗥𝗡𝗜𝗡𝗚 🚸\n<b>🔞 Pʀᴏɴ ᴄᴏɴᴛᴇɴᴛꜱ ʟᴇᴀᴅꜱ ᴛᴏ ᴘᴇʀᴍᴀɴᴇɴᴛ ʙᴀɴ ʏᴏᴜ.</b>\n\n<b>Aʟsᴏ  Gᴇᴛ Aʟʟ Cᴏᴍᴍᴀɴᴅs Jᴜsᴛ Sᴇɴᴅ.</b> /List""",
         
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup(
             [
                 [InlineKeyboardButton("💁‍♂️ Owner", url="https://t.me/Archie_Bots")],
-                [InlineKeyboardButton("💥 Source Code", url="https://t.me/Archie_Bots")]
+                [InlineKeyboardButton("📢 Bot Channel", url="https://t.me/Archie_Bots")]
             ]
         )
     )
